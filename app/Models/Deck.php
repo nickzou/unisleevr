41a55e300c85d4e6c29e1d454f8308c6a1d91678
Nick Zou <z.nick00@gmail.com>
2024-12-30 20:27:45 -0500

    add converts cardId string into array of strings

--- app/Models/Deck.php
+++ app/Models/Deck.php
@@ -3,13 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Card;
 
 class Deck extends Model
 {
     protected $fillable = ['name'];
 
-    // public function user()
-    // {
-    //     return $this->belongsTo(User::class);
-    // }
+    /*For testing purposes, clean up later*/
+    public function cardsViaCli($cardIDs)
+    {
+        if(mb_strlen($cardIDs) <= 0) {
+            echo 'no card IDs supplied';
+        }
+       var_dump(explode(" ", $cardIDs));
+    }
+     public function cards()
+     {
+         return $this->hasMany(Card::class);
+     }
 }
